0938542ca1600352c77a0ba9da727a53f79b2f9a
jderre <64578549+jderre@users.noreply.github.com>
2020-05-28 10:00:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,20 @@
 # python 3
 
 from player import Player
-import random as rd 
+import random as rd
 rd.seed()
 
 
 industrial_consumer = Player()
 
 for t in range(48):
-    load = industrial_consumer.compute_load(t, rd.random()*100)
+    demand = rd.random()*100
+    load = industrial_consumer.compute_load(t, demand)
     assert industrial_consumer.battery_stock[t] >= 0
     assert industrial_consumer.battery_stock[t] <= industrial_consumer.capacity
+    data = {"purchase" : 0.06 ,"sale" : 0.03}
+    imbalance = {"purchase_cover" : 0.5 , "sale_cover" : 1}
+    relative_grid_load=100
+    industrial_consumer.observe(t,demand,data,imbalance,relative_grid_load)
 
 print('test passed')
