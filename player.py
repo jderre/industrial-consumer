5d18cd2fc95f3d779f96717cc60ddb355e1dc371
jderre <64578549+jderre@users.noreply.github.com>
2020-05-28 09:59:52 +0200

    Update player.py

--- player.py
+++ player.py
@@ -9,29 +9,41 @@ class Player:
         self.demand=[]
         self.bill = np.zeros(48) # prix de vente de l'électricité
         self.load= np.zeros(48) # chargement de la batterie (li)
+        self.penalty=np.zeros(48)
+        self.grid_relative_load=np.zeros(48) #global demand- global supply
         self.battery_stock = np.zeros(49) #a(t)
         self.capacity = 100
-        self.max_load = 100
-        self.prices = {"internal" : [],"external_purchase" : [],"external_sale" : []}
-        self.imbalance=[]
+        self.pmax = 100
+        self.prices = {"purchase" : [],"sale" : []}
+        self.imbalance={"purchase_cover":[], "sale_cover": []}
 
     def take_decision(self,time):
-        
         # TO DO:
-        # implement your policy here to return the load charged / discharged in the battery
+        # implement your policy here to return the load charged / discharged in the battery between -pmax and pmax
         # below is a simple example  
-            
-        if time>10 and time<32:
-            return -10
-        else:
-            return +20
+        if self.grid_relative_load[time-1]>-100 and self.grid_relative_load[time-1]<=100:
+            if time<=10 or time<=40:
+                return self.pmax/12
+            else:
+                return -self.pmax/12
+        if self.grid_relative_load[time-1]>100:
+            return -self.pmax/3
+        if self.grid_relative_load[time-1]<-100:
+            return self.pmax/3
+        
+        # if time<=10: #temps correspond à des demi-heures. t dans [0,47]
+        #     return self.pmax/11 #ça correspond à 11 pas de temps de trente minutes
+        # if time>10 and time<40 : #dans ce cas c'est plus compliqué d'évaluer le prix de vente et achat (on a 37 pas de temps)
+        #     return -self.pmax/29
+        # else : 
+        #     return self.pmax/10
 
     def update_battery_stock(self,time,load):
         
         #If the battery isn't enough powerful, the battery load is set to the battery maximum power.
 
-            if abs(load) > self.max_load:
-                load = self.max_load*np.sign(load) 
+            if abs(load) > self.pmax:
+                load = self.pmax*np.sign(load) 
             new_stock = self.battery_stock[time] + (self.efficiency*max(0,load) - 1/self.efficiency * max(0,-load))*self.dt
             
         #If the battery isn't full enough to provide such amount of electricity, 
@@ -59,25 +71,28 @@ class Player:
         
         return self.load[time]
     
-    def observe(self, t, demand, price, imbalance):
+    def observe(self, t, demand, price, imbalance,grid_relative_load):
         self.demand.append(demand)
         
-        self.prices["internal"].append(price["internal"])
-        self.prices["external_sale"].append(price["external_sale"])
-        self.prices["external_purchase"].append(price["external_purchase"])
+        self.prices["purchase"].append(price["purchase"])
+        self.prices["sale"].append(price["sale"])
+
+        self.imbalance["purchase_cover"].append(imbalance["purchase_cover"])
+        self.imbalance["sale_cover"].append(imbalance["sale_cover"])
         
-        self.imbalance.append(imbalance)
-        
+        self.grid_relative_load[t]=grid_relative_load
     
     def reset(self):
         self.load= np.zeros(48)
         self.bill = np.zeros(48)
+        
+        self.penalty=np.zeros(48)
+        self.grid_relative_load=np.zeros(48)
         
         last_bat = self.battery_stock[-1]
         self.battery_stock = np.zeros(49)
         self.battery_stock[0] = last_bat
         
         self.demand=[]
-        self.prices = {"internal" : [],"external_purchase" : [],"external_sale" : []}
-        self.imbalance=[]
-    
+        self.prices = {"purchase" : [],"sale" : []}
+        self.imbalance={"purchase_cover":[], "sale_cover": []}
